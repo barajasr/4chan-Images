4378cc5edcf70e9b222b7d071933ac07fce6b82c
barajasr <barajasr89@gmail.com>
2013-02-02 14:29:49 -0800

    Minor nitpicks.

--- 4chan.images.py
+++ 4chan.images.py
@@ -13,21 +13,21 @@ import urllib
 import subprocess 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', action='store', default='./', dest='directory', help='Directory/path to save images. Default is ./')
-parser.add_argument('-f', '--filenames', action='store_true', default=False, dest='filenames', help='Save files to filenames seen on posting board if changed; files shown with (...) is overlooked.')
+parser.add_argument('-d', '--directory', action='store', default='', dest='directory', help='Directory/path to save images. Default is ./')
+parser.add_argument('-f', '--filenames', action='store_true', default=False, dest='filenames', help='Save files to filenames seen on posting board if different; files shown with (...) is overlooked.')
 parser.add_argument('-q', '--quiet', action='store_true', default=False, dest='quiet', help='Suppress any output, excluding errors.')
 parser.add_argument('-l', '--link', action='store', dest='link', required=True, help='Link to thread with desired images.')
 
 def downloadImages(imageList, directory, quiet):
     for image in imageList:
-        if not quiet : print image[0], '\tDownloading...'   # Would prefer suppressing newline, but blocks
+        if not quiet : print image[0], '\tDownloading...'
         saveFile = open(directory + '/' + image[0], 'wb')
         saveFile.write(urllib.urlopen('http://' + image[1]).read())
         saveFile.close()
         if not quiet: print '\t\t\tSaved.'
 
 def getImageList(html, filenameFlag):
-    """ Should return a list of tuples of 4chan images with (filename, Source) format.
+    """ Should return a list of tuples of 4chan images with (filename, sourceLink) format.
     """
     if filenameFlag:
         return re.findall(r'title="(?P<title>[\S ]+)">(?P=title)\S+ \S+ href="//(\S+)"', html)
@@ -61,14 +61,13 @@ def main():
         print str(len(threadImages)) + " Images Found."
         print "=="*30 + "\n" + "=="*30
 
-    targetDir =  './' + threadNumber[0] if args['directory'] == './' else os.path.join(args['directory'], threadNumber[0])
+    targetDir = threadNumber[0] if args['directory'] == '' else os.path.join(args['directory'], threadNumber[0])
     mkdir(targetDir)
     downloadImages(threadImages, targetDir, quiet)
     if not quiet: print "=="*30 + "\n" + "=="*30
 
 def mkdir(targetDir):
-    if not os.path.isdir(targetDir):
-        subprocess.call(['mkdir', '-p', targetDir])
+    subprocess.call(['mkdir', '-p', targetDir])
 
 if __name__ == "__main__":
 	main()
