de8ebfc30a1fd28546616811225bed2a256e13d1
barajasr <barajasr89@gmail.com>
2013-02-05 01:27:07 -0800

    Argparse options amended. Elimination of mkdir function(1 line vs 3). Slight reorganization of code.

--- 4chan.images.py
+++ 4chan.images.py
@@ -2,7 +2,7 @@
 # 4chan.images.py
 # 
 # Author: Richard Barajas
-# Date: 02-02-2013
+# Date: 05-02-2013
 #
 
 import argparse
@@ -13,19 +13,20 @@ import urllib
 import subprocess 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', action='store', default='', dest='directory', help='Directory/path to save images. Default is ./')
+parser.add_argument('-d', '--directory', action='store', default='', dest='directory', help='Directory to save images. Default is thread number')
 parser.add_argument('-f', '--filenames', action='store_true', default=False, dest='filenames', help='Save files to filenames seen on posting board if different than source link for image.')
+parser.add_argument('-p', '--path', action='store', default='', dest='path', help='Path to save thread to. Default is ./')
 parser.add_argument('-q', '--quiet', action='store_true', default=False, dest='quiet', help='Suppress any output, excluding errors.')
 parser.add_argument('-l', '--link', action='store', dest='link', required=True, help='Link to thread with desired images.')
 
-def downloadImages(imageList, directory, quiet):
+def downloadImages(imageList, path, quiet):
     """Downloads images requested from imageList param and saves to files.
        Progress output is default, set quiet flag if not desired.
     """
     number = 0
     for image in imageList:
         number += 1
-        filename = os.path.join(directory, image[1])
+        filename = os.path.join(path, image[1])
         if os.path.isfile(filename): continue
         if not quiet : 
             sys.stdout.write(str(number) + '. ' + image[1] + '\t\tDownloading...')
@@ -35,13 +36,14 @@ def downloadImages(imageList, directory, quiet):
         saveFile.close()
         if not quiet: sys.stdout.write('\tSaved.\n')
 
-def getImageList(html, filenameFlag):
+def getImageList(threadLink, filenameFlag):
     """ Should return a list of tuples of 4chan images with (sourceLink, filename) format.
     """
+    html = urllib.urlopen(threadLink).read()
     if not filenameFlag:
         return re.findall(r'<a \S+ href="//(\S+/src/(\S+))"', html)
 
-    results = re.findall(r'"((?P<subname>[^"]+)[^"]*(?P<format>\.[a-z]{3,4}))">(?P=subname)(\(\.\.\.\))?(?P=format)\S+ \S+ href="//(\S+)"', html)
+    results = re.findall(r'"((?P<subname>[^"]+)[^"]*(?P<format>[a-z.]{4,5}))">(?P=subname)(\(\.\.\.\))?(?P=format)\S+ \S+ href="//(\S+)"', html)
     if results is not None:
         for i in range(0, len(results)):
             results[i] = (results[i][4], results[i][0])
@@ -51,32 +53,28 @@ def main():
     args = vars(parser.parse_args())
 
     threadLink = args['link']
-    threadNumber = re.findall(r'org/\S+/res/(\d+\Z)', threadLink)
+    threadNumber = re.findall(r'http://boards\S+/res/(\d+\Z)', threadLink)
     if threadNumber is None or threadNumber == []:
         sys.stderr.write('Thread not found, now exiting.\n')
         sys.exit(0)
 
     quiet = args['quiet']
-    if not quiet:
-        print "Thread to Download: " + threadLink
-        print 'Searching for images...'
-    threadBody = urllib.urlopen(threadLink).read()
-    threadImages = getImageList(threadBody, args['filenames'])
+    if not quiet: print 'Searching for images...'
+    threadImages = getImageList(threadLink, args['filenames'])
     if threadImages is None or threadImages == []:
         sys.stderr.write('No images found, now exiting.\n')
         sys.exit(0)
     
-    if not quiet: 
-        print str(len(threadImages)) + " Images Found."
+    threadFolder = threadNumber[0] if args['directory'] == '' else args['directory']
+    fullPath = threadFolder if args['path'] == '' else os.path.join(args['path'], threadFolder)
+    subprocess.call(['mkdir', '-p', fullPath])
+
+    if not quiet:
+        print str(len(threadImages)), "Images Found."
+        print 'Savinag thread to:', fullPath
         print "=="*30 + "\n" + "=="*30
-
-    targetDir = threadNumber[0] if args['directory'] == '' else os.path.join(args['directory'], threadNumber[0])
-    mkdir(targetDir)
-    downloadImages(threadImages, targetDir, quiet)
+    downloadImages(threadImages, fullPath, quiet)
     if not quiet: print "=="*30 + "\n" + "=="*30
-
-def mkdir(targetDir):
-    subprocess.call(['mkdir', '-p', targetDir])
 
 if __name__ == "__main__":
 	main()
