a9a0339e04b6dc7a463ccee5369c48cd79fb3ffa
Richard Barajas <barajasr89@gmail.com>
2014-10-17 20:02:19 -0700

    Cleaning up main. Move logging and fails outside of main.

--- 4chan.images.py
+++ 4chan.images.py
@@ -2,7 +2,7 @@
 # 4chan.images.py
 #
 # Author: Richard Barajas
-# Date: 16-10-2014
+# Date: 17-10-2014
 
 import argparse
 from BeautifulSoup import BeautifulSoup
@@ -24,7 +24,7 @@ parser.add_argument('-n', '--nofolder',
                     help='Do not create a folder to group media resources in.')
 parser.add_argument('-p', '--path', 
                     action='store',
-                    default='', 
+                    default='./', 
                     dest='path',
                     help='Path to save thread to. Default is ./')
 parser.add_argument('-q', '--quiet', 
@@ -36,9 +36,15 @@ parser.add_argument('url',
                     help='Url to thread with desired images.')
 
 def downloadImages(imageList, path, quiet):
-    """Downloads images requested from imageList param and saves to files.
-       Progress output is default, set quiet flag if not desired.
+    """ Downloads images requested from imageList param and saves to files.
+        Progress output is default, set quiet flag if not desired.
     """
+
+    width = 40
+    if not quiet:
+        print str(len(imageList)), 'Images Found.'
+        print 'Saving thread to:', path
+        print '=='*width + '\n' + '=='*width
 
     number = 0
     for image in imageList:
@@ -51,10 +57,12 @@ def downloadImages(imageList, path, quiet):
             message = '\tSaved.\n' if not downloaded else '\tAlready Exists.\n'
             sys.stdout.write(message)
 
+    if not quiet: print '=='*width + '\n' + '=='*width
+
 def frontProgressText(filename, imageNumber):
-    """Function is solely a slave call from downloadImages.
-       Prints the image number, filename, and Downloading text portions
-       of the progress.
+    """ Function is solely a slave call from downloadImages.
+        Prints the image number, filename, and Downloading text portions
+        of the progress.
     """
 
     sys.stdout.write(str(imageNumber) + '. ' + filename)
@@ -66,21 +74,12 @@ def frontProgressText(filename, imageNumber):
     sys.stdout.flush()
 
 def getImageList(threadUrl):
-    """ Should return a list of tuples of 4chan images with (sourceUrl, filename)
-        format.
+    """ Should return a list of tuples of 4chan images with
+        (sourceUrl, filename) format.
+        Fail otherwise.
     """
 
-    try:
-        html = urllib2.urlopen(threadUrl).read()
-    except (urllib2.HTTPError, urllib2.URLError), e:
-        sys.stderr.write('\nError while making request for ' + threadUrl + '\n')
-        if isinstance(e, urllib2.HTTPError):
-            sys.stderr.write(str(e.code) + '\n' + e.reason + '\n')
-        else:
-            sys.stderr.write(e.reason + '\n')
-        sys.exit(1)
-
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(getThreadSource(threadUrl))
     # Expected entry example for findAll result
     # <div class="fileText" id="fT6816862">File: <a href="//i.4cdn.org/sci/1413436856754.jpg" target="_blank">black science man.jpg</a> (81 KB, 960x502)</div>
     data = soup.findAll('div', {'class' : 'fileText'})
@@ -92,10 +91,37 @@ def getImageList(threadUrl):
         if anchor.has_key('title'): # Filename has '(...)' pattern in it
             filename = anchor['title']
         tuples.append(('http:'+ anchor['href'], filename))
+
+    if tuples is None or tuples == []:
+        sys.stderr.write('No images found, now exiting.\n')
+        sys.exit(0)
+
     return tuples
 
+def getThreadNumber(url):
+    """ Returns thread number if url is valid.
+    """
+
+    return validateThreadUrl(url)
+
+def getThreadSource(url):
+    """ Return html of requested thread.
+        Fail otherwise.
+    """
+
+    try:
+        return urllib2.urlopen(url).read()
+    except (urllib2.HTTPError, urllib2.URLError), e:
+        sys.stderr.write('\nError while making request for ' + threadUrl + '\n')
+        if isinstance(e, urllib2.HTTPError):
+            sys.stderr.write(str(e.code) + '\n' + e.reason + '\n')
+        else:
+            sys.stderr.write(e.reason + '\n')
+        sys.exit(1)
+
 def imageToFile(filename, url):
-    """Attempt to download image and store to file.
+    """ Attempt to download image and store to file.
+        Fail otherwise.
     """
 
     try:
@@ -118,23 +144,24 @@ def imageToFile(filename, url):
         sys.exit(2)
 
 def main():
+
     args = vars(parser.parse_args())
 
     threadUrl = args['url']
-    threadNumber = re.findall(r'http://boards\S+/thread/(\d+\Z)', threadUrl)
-    if threadNumber is None or threadNumber == []:
-        sys.stderr.write('Thread not found, now exiting.\n')
-        sys.exit(0)
+    threadNumber = getThreadNumber(threadUrl)
+    fullPath = preparePath(args, threadNumber)
 
     quiet = args['quiet']
     if not quiet: print 'Searching for images...',
-    threadImages = getImageList(threadUrl)
-    if threadImages is None or threadImages == []:
-        sys.stderr.write('No images found, now exiting.\n')
-        sys.exit(0)
+    downloadImages(getImageList(threadUrl), fullPath, quiet)
+
+def preparePath(args, threadNumber):
+    """ Return apporpiate path as determined by args.
+        Fail if path invalid of failure to create directory.
+    """
 
     fullPath = args['path']
-    if fullPath != '' and  not os.path.isdir(fullPath):
+    if fullPath != './' and  not os.path.isdir(fullPath):
             sys.stderr.write( "'"+ fullPath + "' path does not exist or invalid\n")
             sys.exit(3)
     if not args['nofolder']:
@@ -143,15 +170,21 @@ def main():
         if subprocess.call(['mkdir', '-p', fullPath]) != 0:
             sys.stderr.write('Failed to make target directory ' + fullPath + '\n')
             sys.exit(4)
+    return fullPath
 
-    width = 40
-    if not quiet:
-        print str(len(threadImages)), "Images Found."
-        print 'Saving thread to:', fullPath
-        print "=="*width + "\n" + "=="*width
-    downloadImages(threadImages, fullPath, quiet)
-    if not quiet: print "=="*width + "\n" + "=="*width
 
-if __name__ == "__main__":
+def validateThreadUrl(url):
+    """ Checks url pattern; return thread number if possible.
+        Fail otherwise.
+    """
+
+    threadNumber = re.findall(r'http://boards\S+/thread/(\d+\Z)', url)
+    if threadNumber is None or threadNumber == []:
+        sys.stderr.write('Thread not found, now exiting.\n')
+        sys.exit(0)
+
+    return threadNumber
+
+if __name__ == '__main__':
     main()
 
